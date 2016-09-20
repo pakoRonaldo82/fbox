1a0684dfc226d2739df9160efa41bd425ab32382
Francisco Blanco <Francisco.Blanco@blackboard.com>
2016-09-20 08:06:26 -0500

    adding contactanos, como funciona and sabias que templates

---
+++ ui/contactus/contactus.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('Contactus')
+
+.controller('ContactusController',
+    ['$scope', '$rootScope', '$location',
+    function ($scope, $rootScope, $location) {
+     $scope.priceSlider = 100;
+  }]);
